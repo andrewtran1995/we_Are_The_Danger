6969a3c7d89e4fbc39c8d489fd295de3978b44ff
Andrew Tran <andrew.tran345@gmail.com>
2015-04-04 13:20:41 -0700

    Added debugging console logs to saveAll.js.

--- saveAll.js
+++ saveAll.js
@@ -1,18 +1,27 @@
 // Called when user clicks on browser action.
-console.log("Testing.");
-console.log("Testing1.");
-
+console.log("saveAll.js started.");
 
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementByID('button').addEventListener("click", function() {
-        chrome.tabs.query({
-            'active': true,
-            'windowId': chrome.windows.WINDOW_ID_CURRENT
-        }, function(tabs) {
-            console.log("Saving first image from " + tabs[0].url);
-            chrome.tabs.executeScript({
-                code: 'chrome.downloads.download({url: document.images[0].src, conflictAction: "uniquify"}, function () {console.log("Download successful!")});'
-            });
-        });
+    console.log("Event listener added.");
+    var queryInfo = {
+        active: true,
+        currentWindow: true
+    };
+    var tab;
+    chrome.tabs.query(queryInfo, function(tabs) {
+        // Note the differences in "document" and "tab" found through tabs.query.
+        // We have to find a way to search through the DOM of the current tab.
+        tab = tabs[0];
+        // Debugging print statements.
+        console.log("document given by DOM: ");
+        console.log("url: " + document.URL);
+        console.log("tab found through tabs.query:");
+        console.log("windowId: " + tab.windowId +
+                    "\nindex: " + tab.index +
+                    "\nurl: " + tab.url);
+    });
+    //console.log("Saving first image from " + tab.url);
+    chrome.tabs.executeScript({
+        code: 'chrome.downloads.download({url: document.images[0].src, conflictAction: "uniquify"}, function () {console.log("Download successful!")});'
     });
 });
